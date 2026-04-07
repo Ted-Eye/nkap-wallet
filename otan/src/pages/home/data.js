7549a343ed33fdc1186f1b5e73e54839d868a624
Ted-Eye <booktedeye@gmail.com>
2026-04-07 22:52:37 +0100

    minor updates

---
+++ otan/src/pages/home/data.js
@@ -0,0 +1,59 @@
+const transactions = [
+            {
+                id: 1,
+                type: 'cash-in',
+                amount: 70000,
+                motive: 'salary',
+                cashInDate: null
+            },
+
+            {
+                id: 2,
+                type: 'cash-in',
+                amount: 3000,
+                motive: 'njangi',
+                cashInDate: null
+            },
+
+            {
+                id: 3,
+                type: 'cash-in',
+                amount: 5000,
+                motive: 'sale of TV',
+                cashInDate: null
+            },
+
+            {
+                id: 4,
+                type: 'cash-out',
+                amount: 5000,
+                motive: 'house rent',
+                cashInDate: null
+            },
+
+            {
+                id: 5,
+                type: 'cash-out',
+                amount: 500,
+                motive: 'electric bill',
+                cashInDate: null
+            },
+
+            {
+                id: 6,
+                type: 'cash-out',
+                amount: 1500,
+                motive: 'medical bill',
+                cashInDate: null
+            },
+
+            {
+                id: 7,
+                type: 'cash-out',
+                amount: 2000,
+                motive: 'food',
+                cashInDate: null
+            },
+]
+
+export default transactions;
